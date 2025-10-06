2f51671b81fde6c41a38dae197fc8efc0700a7a9
ngoctinn <Tinvg2918@gmail.com>
2025-10-06 17:22:07 +0700

    feat: add additional remote pattern for placehold.co in image configuration

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -9,7 +9,14 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
     ],
+    dangerouslyAllowSVG: true,
   },
 };
 
